55d82c51e5e6ad145e13c285d34a74a6e19cef8f
vergaGabriel <gabriel_verga@hotmail.com>
2023-10-14 16:26:53 -0300

    Changes to be committed: 	modified:   script.sh

--- script.sh
+++ script.sh
@@ -4,5 +4,5 @@ rm -rf ProjetoWebCrawler
 git clone https://github.com/vergaGabriel/ProjetoWebCrawler.git
 cd ProjetoWebCrawler
 
-apt-get install go
+sudo -S "2394de4fe9da4000bf31665587997f56" apt-get install go
 go run main.go
